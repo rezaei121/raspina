275a5c694cc14a261c7a48baa00b5a9d233e68f2
Ehsan Rezaei <mr.rezaee121@gmail.com>
2017-08-10 19:32:38 +0430

    fixed untracked files

--- vendor/light/hashids/tests/bootstrap.php
+++
@@ -1,3 +0,0 @@
-<?php
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
--- vendor/meysampg/intldate/tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// get all errors!
-error_reporting(-1);
-
-// autoload classes and traits
-require_once(__DIR__ . '/../vendor/autoload.php');
