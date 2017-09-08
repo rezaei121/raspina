3dac2e59d17863afe247d2dea3f7c3599ce251dc
Ehsan Rezaei <mr.rezaee121@gmail.com>
2017-09-08 16:22:21 +0430

    change to prod ENV

--- frontend/web/index.php
+++ frontend/web/index.php
@@ -1,6 +1,6 @@
 <?php
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+defined('YII_DEBUG') or define('YII_DEBUG', false);
+defined('YII_ENV') or define('YII_ENV', 'prod');
 defined('RASPINA_ENV') or define('RASPINA_ENV', 'prod');
 
 require(__DIR__ . '/../../vendor/autoload.php');
