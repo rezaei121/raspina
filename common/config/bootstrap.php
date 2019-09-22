35aac57684c48601479c440c593d076cf7e9d883
rezaei121 <mr.rezaee121@gmail.com>
2019-09-22 21:34:01 +0330

    remove old files.

--- common/config/bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-Yii::setAlias('@common', dirname(__DIR__));
-Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
-Yii::setAlias('@dashboard', dirname(dirname(__DIR__)) . '/dashboard');
-Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
--- dashboard/components/widgets/ActiveForm.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace dashboard\components\widgets;
-
-use Yii;
-
-class ActiveForm extends \yii\widgets\ActiveForm
-{
-    public $fieldClass = 'dashboard\components\widgets\ActiveField';
-}
